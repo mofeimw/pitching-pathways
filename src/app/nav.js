e908956dd25b1beeb59b1ed767c216406d464ddc
mofeimw <mofei.mw@gmail.com>
2023-10-30 21:02:24 -0400

    initial commit - coming soon page

---
+++ src/app/nav.js
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import styles from './nav.module.css'
+
+export default function Nav() {
+  return (
+    <header className={styles.nav}>
+      <a href="#"><Image
+        src="/logo.png"
+        className={styles.navLogo}
+        width={320}
+        height={220}
+      /></a>
+      <ul className={styles.navList}>
+        <li className={styles.navListEl}><a href="#">Home</a></li>
+        <li className={styles.navListEl}><a href="#">About</a></li>
+        <li className={styles.navListEl}><a href="#">Team</a></li>
+        <li className={styles.navListEl}><a href="#">Join</a></li>
+      </ul>
+    </header>
+  )
+}
---
+++ src/app/socialfooter.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import styles from './socialfooter.module.css'
+
+export default function SocialFooter() {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.footerList}>
+        <li><a href="#"><Image
+          src="/instagram.svg"
+          width={40}
+          height={40}
+        /></a></li>
+        <li><a href="#"><Image
+          src="/twitter.svg"
+          width={40}
+          height={40}
+        /></a></li>
+        <li><a href="#"><Image
+          src="/facebook.svg"
+          width={40}
+          height={40}
+        /></a></li>
+      </ul>
+    </footer>
+  )
+}
