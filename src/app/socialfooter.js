263f36fd01ef20281aab1e3314285dc6baf3648d
PHHua <149897610+PHHua@users.noreply.github.com>
2023-11-09 20:30:10 -0600

    Social media Footer linked to twitter and instagram

--- src/app/socialfooter.js
+++ src/app/socialfooter.js
@@ -5,17 +5,17 @@ export default function SocialFooter() {
   return (
     <footer className={styles.footer}>
       <ul className={styles.footerList}>
-        <li><a href="#"><Image
+        <li><a href="https://www.instagram.com/pitchingpathways/"><Image
           src="/instagram.svg"
           width={40}
           height={40}
         /></a></li>
-        <li><a href="#"><Image
+        <li><a href="https://twitter.com/PitchingPathway"><Image
           src="/x.svg"
           width={40}
           height={40}
         /></a></li>
-        <li><a href="#"><Image
+        <li><a href=""><Image
           src="/facebook.svg"
           width={40}
           height={40}
