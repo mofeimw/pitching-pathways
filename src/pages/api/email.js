f8c15af5915291e382e7e7dbdc2c20dfc70c6cad
mofeimw <mofei.mw@gmail.com>
2023-11-13 12:51:50 -0500

    push to right end of deque

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -3,7 +3,7 @@ import { kv } from '@vercel/kv';
 export default async function handler(request, response) {
     if (request.method === "POST") {
         let email = request.body.email;
-        kv.lpush("emails", email);
+        kv.rpush("emails", email);
         return response.status(200).json(email);
     } else if (request.method === "GET") {
         let emails = await kv.lrange("emails", 0, -1);
